20152342f02427c12cf8e80e9b2f9b788785ef36
Maksym <maxthebest1906@gmail.com>
2022-02-03 11:23:02 +0200

    Code spliting is ready with Suspence and lazy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import React from 'react'; //, { Suspense, lazy }
+import React, { Suspense, lazy } from 'react';
 import { Route, Switch } from "react-router-dom";
 import routes from "./routes";
-import HomeView from './views/HomeView';
-import MoviesDetails from './views/MoviesDetails';
-import MoviesView from './views/MoviesView';
+
 import AppBar from './components/AppBar';
+import Loader from './components/Loader';
+
+const HomeView = lazy(() =>
+  import('./views/HomeView.jsx' /* webpackChunkName: "home-view" */)
+);
+
+const MoviesDetailsView = lazy(() =>
+  import('./views/MoviesDetailsView.jsx' /* webpackChunkName: "movies-details-view" */)
+);
+
+const MoviesView = lazy(() =>
+  import('./views/MoviesView.jsx' /* webpackChunkName: "movies-view" */)
+);
+
+const NotFoundView = lazy(() =>
+  import('./views/NotFoundView.jsx' /* webpackChunkName: "not-found-view" */)
+);
 
 function App() {
   return (
     <>
     <AppBar />
-    {/* <Suspense fallback={<h1>Loading...</h1>}> */}
+    <Suspense fallback={<Loader/>}>
       <Switch>
         <Route exact path={routes.home} component={HomeView}/>
-        <Route path={routes.moviesDetails} component={MoviesDetails}/>
+        <Route path={routes.moviesDetails} component={MoviesDetailsView}/>
         <Route path={routes.movies} component={MoviesView}/> 
+        <Route component={NotFoundView} />
       </Switch>
-    {/* </Suspense> */}
+    </Suspense>
     </>
   );
 }
---
+++ src/views/NotFoundView.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const NotFoundView = props => {
+    return (
+       <h1>404 NOT FOUND!</h1>
+    );
+};
+export default NotFoundView;
